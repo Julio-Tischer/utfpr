03477df8a060d9a4c38e5bcc8c7081551771ebaa
Julio-Tischer <juliotischer@alunos.utfpr.edu.br>
2025-05-07 09:18:33 -0300

    iran.c
    lua <3

---
+++ iran.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define 數位 數位
+#define a "a"
+#define b "b"
+#define c "c"
+#define d "d"
+#define e "e"
+#define j "i"
+
+
+int وظيفة(int x){
+    return x;
+}
+
+int main(){
+    int 數位;
+    printf("me de um numero");
+    scanf("%d",&數位);
+    printf("\n\n\n Seu number é %d",وظيفة(數位));
+
+    printf(a b c d);
+}
