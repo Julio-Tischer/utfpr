fcc78a76b0d8d02a8033797b848abe719279c587
augustorics <bboeissa@gmail.com>
2025-05-07 09:49:05 -0300

    Add files via upload

--- iran.c
+++ iran.c
@@ -28,6 +28,132 @@
 #define z "z"
 #define espaço " "
 
+#define BA "ba"
+#define BE "be"
+#define BI "bi"
+#define BO "bo"
+#define BU "bu"
+
+#define CA "ca"
+#define CE "ce"
+#define CI "ci"
+#define CO "co"
+#define CU "cu"
+
+#define DA "da"
+#define DE "de"
+#define DI "di"
+#define DO "do"
+#define DU "du"
+
+#define FA "fa"
+#define FE "fe"
+#define FI "fi"
+#define FO "fo"
+#define FU "fu"
+
+#define GA "ga"
+#define GE "ge"
+#define GI "gi"
+#define GO "go"
+#define GU "gu"
+
+#define HA "ha"
+#define HE "he"
+#define HI "hi"
+#define HO "ho"
+#define HU "hu"
+
+#define JA "ja"
+#define JE "je"
+#define JI "ji"
+#define JO "jo"
+#define JU "ju"
+
+#define KA "ka"
+#define KE "ke"
+#define KI "ki"
+#define KO "ko"
+#define KU "ku"
+
+#define LA "la"
+#define LE "le"
+#define LI "li"
+#define LO "lo"
+#define LU "lu"
+
+#define MA "ma"
+#define ME "me"
+#define MI "mi"
+#define MO "mo"
+#define MU "mu"
+
+#define NA "na"
+#define NE "ne"
+#define NI "ni"
+#define NO "no"
+#define NU "nu"
+
+#define PA "pa"
+#define PE "pe"
+#define PI "pi"
+#define PO "po"
+#define PU "pu"
+
+#define QA "qa"
+#define QE "qe"
+#define QI "qi"
+#define QO "qo"
+#define QU "qu"
+
+#define RA "ra"
+#define RE "re"
+#define RI "ri"
+#define RO "ro"
+#define RU "ru"
+
+#define SA "sa"
+#define SE "se"
+#define SI "si"
+#define SO "so"
+#define SU "su"
+
+#define TA "ta"
+#define TE "te"
+#define TI "ti"
+#define TO "to"
+#define TU "tu"
+
+#define SA "sa"
+#define SE "se"
+#define SI "si"
+#define SO "so"
+#define SU "su"
+
+#define VA "va"
+#define VE "ve"
+#define VI "vi"
+#define VO "vo"
+#define VU "vu"
+
+#define WA "wa"
+#define WE "we"
+#define WI "wi"
+#define WO "wo"
+#define WU "wu"
+
+#define XA "xa"
+#define XE "xe"
+#define XI "xi"
+#define XO "xo"
+#define XU "xu"
+
+#define ZA "za"
+#define ZE "ze"
+#define ZI "zi"
+#define ZO "zo"
+#define ZU "zu"
+
 int وظيفة(int ظي){
     return ظي;
 }
@@ -39,9 +165,10 @@ int main(){
     printf("Шахтерское ремесло");
     return 666;
     }
-    else
+    else{
     printf("\n\n\n Seu number é %d \n",وظيفة(數位));
-
+}
 
     printf(v a i espaço t o m a n d o espaço);
+    printf(BA TA TA espaço QU e i JO espaço MA s SA espaço MO l HO );
 }
